a8a89f4cdeb03a98340b5893e2c4ba3b4be81962
Bohdan Vorobiei <vbs@ukr.net>
2025-11-04 06:35:16 +0200

    task completed

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,5 +1,13 @@
 package mate.academy
 
 class LetterRemover {
+    companion object {
+        fun removeLetter(input: String): Array<Char> {
+            return input
+                .lowercase()
+                .map { if (it == 'r') '!' else it }
+                .toTypedArray()
+        }
+    }
     // implement ...
 }
